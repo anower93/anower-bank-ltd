7c149e07928e7faeab7d181c85922fcb13121195
anower93 <anowerhossin1993@gmail.com>
2021-01-29 15:02:48 +0600

    backend update

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('login').addEventListener('click', function(){
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display="none";
+    const transactionArea = document.getElementById('transaction-area');
+    transactionArea.style.display="block";
+})
